e9069eca2407c98750cf906c863b7377c89d9411
Roman Kozak <cossack.roman@gmail.com>
2022-12-02 18:20:51 +0200

    initial commit

---
+++ - інфузійна терапія, критичні стани -/- гідратація, кислотно-основний стан -/Баланс електролітів.mm
@@ -0,0 +1,36 @@
+<map version="0.9.0">
+  <!--Export from the Blumind, download free mind mapping software Blumind from http://www.blumind.org-->
+  <!--To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net-->
+  <node BACKGROUND_COLOR="#C0C0C0" COLOR="#000000" POSITION="left" TEXT="Center Topic" HGAP="12" VGAP="12">
+    <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="натрій" HGAP="2" VGAP="2">
+      <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="+" HGAP="2" VGAP="2">
+        <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="сопровождается перераспределением жидкости из внутриклеточного во внеклеточный сектор" HGAP="2" VGAP="2" />
+        <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="дегидратации клеток" HGAP="2" VGAP="2" />
+        <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="гіпокалімія" HGAP="2" VGAP="2" />
+        <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="можливо, гіпергідратація" HGAP="2" VGAP="2" />
+        <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="гіперволемія" HGAP="2" VGAP="2">
+          <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="як наслідок - серцева недостатність" HGAP="2" VGAP="2" />
+        </node>
+      </node>
+      <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="-" HGAP="2" VGAP="2">
+        <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="сопровождается гипергидратацией клеток" HGAP="2" VGAP="2" />
+        <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="уменьшением объема циркулирующей крови." HGAP="2" VGAP="2" />
+      </node>
+      <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="MP18-777-3P2B-симптоми" HGAP="2" VGAP="2">
+        <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="жажда" HGAP="2" VGAP="2" />
+      </node>
+    </node>
+    <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="хлор" HGAP="2" VGAP="2">
+      <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="+" HGAP="2" VGAP="2">
+        <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="развитии отечного синдрома" HGAP="2" VGAP="2" />
+      </node>
+      <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="-" HGAP="2" VGAP="2">
+        <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="алкалоза" HGAP="2" VGAP="2" />
+        <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="дегидратация" HGAP="2" VGAP="2" />
+        <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="нарушение дыхания" HGAP="2" VGAP="2" />
+        <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="нарушение сердечной деятельности;" HGAP="2" VGAP="2" />
+        <node BACKGROUND_COLOR="#D3D3D3" COLOR="#000000" POSITION="left" TEXT="возможно развитие судорожного или коматозного состояния с летальным исходом." HGAP="2" VGAP="2" />
+      </node>
+    </node>
+  </node>
+</map>
